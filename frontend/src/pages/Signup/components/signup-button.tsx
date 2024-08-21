a263368ab323b5c8de7cfdfbdce8011bc5cfabb8
sreecharan-desu <sreecharan309@gmail.com>
2024-08-21 21:49:15 +0530

    Completed signup and signin

--- frontend/src/pages/Signup/components/signup-button.tsx
+++ frontend/src/pages/Signup/components/signup-button.tsx
@@ -1,9 +1,14 @@
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { generate_message, message, message_status, signupEmail, signupPassword, signupUsername } from "../../store/store";
+import {
+    generate_message,
+    message,
+    message_status,
+    signupEmail,
+    signupPassword,
+    signupUsername
+} from "../../store/store";
 
-
-export default function Button(){
-
+export default function Button() {
     const username = useRecoilValue(signupUsername);
     const email = useRecoilValue(signupEmail);
     const password = useRecoilValue(signupPassword);
@@ -12,24 +17,25 @@ export default function Button(){
     const setMessage = useSetRecoilState(message);
     const setMessageStatus = useSetRecoilState(message_status);
 
-    const DisplayMessage = (message:string)=>{
+    const displayMessage = (message: string) => {
         setMessage(message);
         setMessageStatus(false); // code:red
         setGenerateMessage(true);
+
         setTimeout(() => {
             setGenerateMessage(false);
             setMessage('');
             setMessageStatus(true); // code:green
         }, 3000);
-    }
+    };
 
     const sendDataToBackend = () => {
         if (!email.includes('@')) {
-            DisplayMessage('Invalid Email')
-        }else{
+            displayMessage('Invalid Email');
+        } else {
             try {
                 const sendData = async () => {
-                    const data = {username, email, password};
+                    const data = { username, email, password };
                     console.log(JSON.stringify(data));
 
                     const res = await fetch('------------------------------------------------', {
@@ -43,14 +49,23 @@ export default function Button(){
                     const result = await res.json();
                     console.log(result);
                 };
+
                 sendData();
             } catch (e) {
-                DisplayMessage('Error sending data to the backend, please try again!');
+                displayMessage('Error sending data to the backend, please try again!');
             }
         }
     };
 
-    return(<>
-        <input type={'button'} value={"Signup"} className="p-2 m-2 rounded-md" style={{ border: '2px solid black' }} onClick={sendDataToBackend}/>                        
-    </>)
+    return (
+        <>
+            <input
+                type="button"
+                value="Signup"
+                className="p-2 m-2 rounded-md"
+                style={{ border: '2px solid black' }}
+                onClick={sendDataToBackend}
+            />
+        </>
+    );
 }

3ed324c561f86406bcf49425569cc6208898b4b1
sreecharan-desu <sreecharan309@gmail.com>
2024-08-22 15:11:13 +0530

    added api's

--- frontend/src/pages/apis/apis.tsx
+++ frontend/src/pages/apis/apis.tsx
@@ -1,8 +1,7 @@
 
-export const CREATE_SPACE_API = '' //post
-export const FETCH_SPACES_API = '' //post
-export const GET_USER_ROLLNUMBER_API = '' //get
-export const USER_SIGNIN_API = ''
-export const VERIFY_OTP_API = ''
-export const USER_SIGNUP_API = ''
-export const api6 = ''
+export const CREATE_SPACE_API = '' //POST
+export const FETCH_SPACES_API = '' //POST
+export const GET_USER_ROLLNUMBER_API = '' //GET
+export const USER_SIGNIN_API = '' //POST
+export const VERIFY_OTP_API = '' //POST
+export const USER_SIGNUP_API = '' //POST

0f6316cafa24a0eae046e579ac2c880d703a55da
sreecharan-desu <sreecharan309@gmail.com>
2024-08-24 20:50:06 +0530

    added api's

--- frontend/src/pages/apis/apis.tsx
+++ frontend/src/pages/apis/apis.tsx
@@ -1,14 +1,18 @@
+export const USER_SIGNUP_API =
+  "/https://study-space-api.vercel.app/api/v1/user/signup";
+export const VERIFY_OTP_API =
+  "/https://study-space-api.vercel.app/api/v1/user/verifysecuritycode";
+export const USER_SIGNIN_API =
+  "/https://study-space-api.vercel.app/api/v1/user/signin";
+export const JOIN_SPACE_API =
+  "/https://study-space-api.vercel.app/api/v1/user/joinspace";
 export const CREATE_SPACE_API =
   "/https://study-space-api.vercel.app/api/v1/user/createspace";
 export const FETCH_SPACES_API =
   "/https://study-space-api.vercel.app/api/v1/user/getspaces";
-export const GET_USER_ROLLNUMBER_API =
+export const GET_USERNAME_API =
   "/https://study-space-api.vercel.app/api/v1/user/getusername";
-export const USER_SIGNIN_API =
-  "/https://study-space-api.vercel.app/api/v1/user/signin";
-export const VERIFY_OTP_API =
-  "/https://study-space-api.vercel.app/api/v1/user/verifysecuritycode";
-export const USER_SIGNUP_API =
-  "/https://study-space-api.vercel.app/api/v1/user/signup";
-export const JOIN_SPACE_API =
-  "/https://study-space-api.vercel.app/api/v1/user/joinspace";
+export const JOINED_SPACES_API =
+  "/https://study-space-api.vercel.app/api/v1/user/spacesjoined";
+export const CREATED_SPACES_API =
+  "/https://study-space-api.vercel.app/api/v1/user/spacescreated";

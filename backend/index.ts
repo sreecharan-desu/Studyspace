57759e4654fddd70e009598cbcd6e355014e254f
sreecharan-desu <sreecharan309@gmail.com>
2024-08-24 12:20:42 +0530

    deployed backend to vercel

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,22 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import { PORT_NO } from './constants';
-import cors from 'cors';
-import { mainRoute } from './routes/mainRoute';
+import express, { Application, Request, Response, NextFunction } from "express";
+import { PORT_NO } from "./constants";
+import cors from "cors";
+import { mainRoute } from "./routes/mainRoute";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/v1', mainRoute);
+app.use("/api/v1", mainRoute);
 
-app.listen(PORT_NO, () => console.log(`Server is listening on port ${PORT_NO}...`));
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
+app.listen(PORT_NO, () =>
+  console.log(`Server is listening on port ${PORT_NO}...`)
+);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(`Error occurred: ${err.message}`);
-    res.status(500).json({ message: "Something went wrong, Charan!" });
+  console.error(`Error occurred: ${err.message}`);
+  res.status(500).json({ message: "Something went wrong, Charan!" });
 });

c78b9d93d676b8e5c02657a8b4c34cfc9e8baacd
sreecharan-desu <sreecharan309@gmail.com>
2024-08-24 20:10:12 +0530

    added api's

--- frontend/src/pages/apis/apis.tsx
+++ frontend/src/pages/apis/apis.tsx
@@ -1,7 +1,14 @@
-export const CREATE_SPACE_API = '/' //POST
-export const FETCH_SPACES_API = '/' //POST
-export const GET_USER_ROLLNUMBER_API = '/' //GET
-export const USER_SIGNIN_API = '/' //POST
-export const VERIFY_OTP_API = '/' //POST
-export const USER_SIGNUP_API = '/' //POST
-export const JOIN_SPACE_API = '/' //POST
+export const CREATE_SPACE_API =
+  "/https://study-space-api.vercel.app/api/v1/user/createspace";
+export const FETCH_SPACES_API =
+  "/https://study-space-api.vercel.app/api/v1/user/getspaces";
+export const GET_USER_ROLLNUMBER_API =
+  "/https://study-space-api.vercel.app/api/v1/user/getusername";
+export const USER_SIGNIN_API =
+  "/https://study-space-api.vercel.app/api/v1/user/signin";
+export const VERIFY_OTP_API =
+  "/https://study-space-api.vercel.app/api/v1/user/verifysecuritycode";
+export const USER_SIGNUP_API =
+  "/https://study-space-api.vercel.app/api/v1/user/signup";
+export const JOIN_SPACE_API =
+  "/https://study-space-api.vercel.app/api/v1/user/joinspace";

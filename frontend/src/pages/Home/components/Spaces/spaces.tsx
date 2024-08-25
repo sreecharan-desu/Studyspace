544e7666151a62788412e0152d7b138b094b4415
sreecharan-desu <sreecharan309@gmail.com>
2024-08-25 11:47:24 +0530

    figured things out

--- frontend/src/pages/Home/components/Spaces/spaces.tsx
+++ frontend/src/pages/Home/components/Spaces/spaces.tsx
@@ -41,7 +41,7 @@ export default function Spaces() {
 
         const data = await res.json();
         console.log(data);
-        if (Array.isArray(data.spaces.spaces)) {
+        if (Array.isArray(data.spaces)) {
           setSpaces(data.spaces.spaces); // Ensure data.spaces is of type Space[]
         } else {
           throw new Error("Unexpected response format");

4d76fcba8a88c9fcabfae12cb8af19e3b792ee3a
sreecharan-desu <sreecharan309@gmail.com>
2024-09-04 21:38:38 +0530

    moved things around

--- frontend/src/pages/Home/components/Spaces/spaces.tsx
+++ frontend/src/pages/Home/components/Spaces/spaces.tsx
@@ -96,7 +96,15 @@ export default function Spaces() {
     return (
       <>
         {localStorage.getItem("token") ? (
-          <TopBar />
+          <>
+            <TopBar />
+
+            <div className="m-10">
+              <Suspense fallback={<div>Loading Heading...</div>}>
+                <Heading text="/Today's saces" />
+              </Suspense>
+            </div>
+          </>
         ) : (
           <>
             <div className="m-10 font-bold text-2xl text-center first-letter:text-4xl md:m-20 md:text-4xl md:first-letter:text-6xl">
@@ -104,7 +112,7 @@ export default function Spaces() {
             </div>
             <div className="m-10">
               <Suspense fallback={<div>Loading Heading...</div>}>
-                <Heading text="/Join Spaces" />
+                <Heading text="/Today's Spaces" />
               </Suspense>
             </div>
           </>
